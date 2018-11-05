EDM v0.4 : Correct Jacobians, add Directional Derivatives, add Rossler.py

EDM.py
@@ -1,4 +1,3 @@
-
 # Python distribution modules
 from collections import OrderedDict
 
@@ -132,10 +131,10 @@ def Prediction( embedding, colNames, target, args ):
                    " dimensions of " + args.inputFile + " Tp=" +\
                    str( args.Tp ) + " θ=" + str( args.theta ) )
         
-        Predictions, Coeff, Jacobians = SMapProjection( libraryMatrix,
-                                                        predictionMatrix,
-                                                        target, neighbors,
-                                                        distances, args )
+        Predictions, Coeff, Jacobians, Tangents = \
+            SMapProjection( libraryMatrix, predictionMatrix,
+                            target, neighbors,
+                            distances, args )
 
     else :
         raise RuntimeError( "Prediction() Invalid projection method: ",
@@ -179,6 +178,12 @@ def Prediction( embedding, colNames, target, args ):
                 Jacobians      = np.append(Jacobians, forecast_Jacob, axis = 0)
                 Jacobians[0 : args.Tp, :] = np.full(Jacobians.shape[1], np.nan)
 
+                forecast_Tangents = Tangents[ (N_pred - args.Tp) : N_pred, : ]
+                Tangents          = np.roll( Tangents, args.Tp, axis = 0 )
+                Tangents          = np.append( Tangents, forecast_Tangents,
+                                               axis = 0)
+                Tangents[0 : args.Tp, :] = np.full(Tangents.shape[1], np.nan)
+
     # Combine into one matrix, create header and write output
     output = np.stack( ( Time, Observations, Predictions ), axis = -1 )
     header = 'Time,Data,Prediction_t(+{0:d})'.format( args.Tp )
@@ -189,18 +194,29 @@ def Prediction( embedding, colNames, target, args ):
 
     smap_output = None
     if args.outputSmapFile and 'smap' in args.method.lower() :
-        # Combine Time, Coeff into one matrix, create header 
+        # Combine Time, Coeff into one matrix
         smap_output = np.hstack( ( Time.reshape(( Time.shape[0], 1 )), Coeff ) )
+        
+        # Create .csv file header
         coef_header = 'Time,'
         for col in range( args.E + 1 ): # C0,C1,C2,...
             coef_header = coef_header + ( 'C{:d},'.format( col ) )
         
         if len( args.jacobians ) :
-            # Append jacobian columns to smap_output
-            smap_output = np.hstack( ( smap_output, Jacobians ) )
+            # Append jacobian and tangent columns to smap_output
+            smap_output = np.hstack( ( smap_output, Jacobians, Tangents ) )
+            
+            # Append Jacobian labels to header
             for pair in args.jacobians :
                 coef_header = coef_header + \
                               ( '∂C{:d}/∂C{:d},'.format( pair[0], pair[1] ) )
+            # Append Tangents labels to header
+            for pair in args.jacobians :
+                coef_header = coef_header + \
+                  ( 'C{:d}(∂/∂C{:d})+C{:d}(∂/∂C{:d}),'.format( pair[0],
+                                                               pair[0],
+                                                               pair[1],
+                                                               pair[1] ) )
 
         coef_header = coef_header[ 0 : -1 ] # remove trailing ,
         
@@ -252,7 +268,7 @@ def Prediction( embedding, colNames, target, args ):
             plotColors = [ 'blue', 'green', 'red', 'magenta', 'brown', 'black',
                 'darkblue', 'darkgreen', 'darkred', 'olive', 'orange', 'gray' ]
             
-            # Note that ax2 is an array of length nrows
+            # Note that ax2 is an array of length args.E + 1
             fig2, ax2 = plt.subplots( nrows = args.E + 1, ncols = 1,
                                       sharex = True,
                                       figsize = ( args.figureSize[0],
@@ -260,6 +276,10 @@ def Prediction( embedding, colNames, target, args ):
                                       dpi = 150 )
             
             for f in range( args.E + 1 ) :
+                # JP: scientific notation on coefficient y axis
+                ax2[f].ticklabel_format( axis = 'y', style = 'sci',
+                                         scilimits=(0,0) )
+                
                 ax2[f].plot( Time, Coeff[ :, f ],
                              label = 'S-Map Coefficients_{0:d}'.format(f),
                              linewidth = 2, color = plotColors[f] )
@@ -272,32 +292,46 @@ def Prediction( embedding, colNames, target, args ):
                           title  = args.inputFile +\
                                    '  E=' + str( args.E  ) +\
                                    ' Tp=' + str( args.Tp ) +\
+                                  r' $\theta$=' + str( args.theta ) +\
                                   r' $\rho$=' + str( round( rho, 3 ) ) )
 
             #-------------------------------------------------------
-            # Plot S-Map jacobians
+            # Plot S-Map Jacobians and Tangents
             if len( args.jacobians ) :
-                fig3, ax3 = plt.subplots( nrows = len( args.jacobians ),
+                # Note that ax3 is an array of len(args.jacobians) * 2
+                nJacobians = len(args.jacobians)
+                fig3, ax3 = plt.subplots( nrows = nJacobians * 2,
                                           ncols = 1, sharex = True,
                                           figsize = ( args.figureSize[0],
                                                       args.figureSize[0] ),
                                           dpi = 150 )
-                
-                for f in range( len( args.jacobians ) ) :
-                    pair = args.jacobians[ f ]
-                    ax3[f].plot( Time, Jacobians[ :, f ],
-                                 label='S-Map ∂C{:d}/∂C{:d}'.format( pair[0],
-                                                                     pair[1] ),
+
+                for i, f in zip( range( nJacobians ),
+                                 range( 0, nJacobians * 2, 2 ) ) :
+                    pair = args.jacobians[ i ]
+                        
+                    ax3[f].plot( Time, Jacobians[ :, i ],
+                                 label='S-Map ∂C{:d}/∂C{:d}'.format(pair[0],
+                                                                    pair[1]),
                                  linewidth = 2, color = plotColors[f] )
-            
+                        
+                    ax3[f].set_ylim([-10, 10])
                     ax3[f].legend()
 
-                ax3[ len( args.jacobians ) - 1 ].set( xlabel = args.plotXLabel )
+                    ax3[f+1].plot( Time, Tangents[ :, i ],
+                        label='C{:d}(∂/∂C{:d})+C{:d}(∂/∂C{:d}),'.format(
+                        pair[0], pair[0], pair[1], pair[1] ),
+                                   linewidth = 2, color = plotColors[f] )
+
+                    ax3[f+1].legend()
+
+                ax3[ nJacobians * 2 - 1 ].set( xlabel = args.plotXLabel )
             
                 ax3[ 0 ].set( ylabel = args.plotYLabel,
                               title  = args.inputFile +\
                               '  E=' + str( args.E  ) +\
                               ' Tp=' + str( args.Tp ) +\
+                              r' $\theta$=' + str( args.theta ) +\
                               r' $\rho$=' + str( round( rho, 3 ) ) )
                 
         plt.show()
@@ -340,6 +374,7 @@ def SMapProjection( libraryMatrix, predictMatrix, target,
     predictions  = np.zeros( N_row )
     coefficients = np.full( ( N_row, args.E + 1 ), np.nan )
     jacobians    = None
+    tangents     = None
 
     for row in range( N_row ) :
         
@@ -407,11 +442,19 @@ def SMapProjection( libraryMatrix, predictMatrix, target,
 
         # Note that args.jacobians is a list of pairs (tuples)
         jacobians = np.zeros( ( N_row, len( args.jacobians ) ) )
+        tangents  = np.zeros( ( N_row, len( args.jacobians ) ) )
         
         for pair, col in zip( args.jacobians, range( len(args.jacobians) ) ) :
-            # gradient over axis = 1, columns
-            jacobians[ :, col ] = np.gradient( coefficients[ :, pair ],
-                                               axis = 1 )[ :, 0]
+            c1 = coefficients[ :, pair[0] ]
+            c2 = coefficients[ :, pair[1] ]
+
+            # Jacobians
+            dc1 = np.gradient( c1 )
+            dc2 = np.gradient( c2 )
+            jacobians[ :, col ] = dc1 / dc2
+
+            # Directional Derivatives (manifold tangent)
+            tangents[ :, col ] = c1 * dc1  + c2 * dc2
 
     if args.Debug:
         print( "SMapProjection() predictions:" )
@@ -420,7 +463,7 @@ def SMapProjection( libraryMatrix, predictMatrix, target,
         if args.jacobians:
             print( np.round( jacobians  [ 0:5, : ], 4 ) )
 
-    return ( predictions, coefficients, jacobians )
+    return ( predictions, coefficients, jacobians, tangents )
     
 #----------------------------------------------------------------------------
 #

data/Rossler.py
@@ -11,39 +11,18 @@ import numpy as np
 #----------------------------------------------------------------------------
 def main():
     '''
-    Create, plot and write out multidimensional data from the Lorenz '96
+    Create, plot and write out multidimensional data from the Rossler
     dynamical model.
 
-    From Lorenz '96:
-
-    N variables k=1, ... N, governed by N equations:
-           d X_k / dt = -X_k-2 * X_k-1 + X_k-1 * X_k+1 - X_k + F
-                      = (X_k+1 - X_k-2) * X_k-1 - X_k + F
-
-    We assume N > 3; the dynamics are of little interest otherwise.
-
-    For very small values of F, all solutions converge to the steady state
-    solution X = F. For larger F most solutions are periodic, but for still
-    larger values of F (dependent on K) chaos ensues. For N = 36 and F = 8
-    λ_1 corresponds to a doubling time of 2.1 days. If F is 10 the time drops
-    to 1.5 days. ... this scaling makes time unit equal to 5 days. With a
-    time step of Δt = 0.05 units, or 6 hours. 
-
-    --
-    Lorenz, Edward (1996). Predictability – A problem partly solved,
-    Seminar on Predictability, Vol. I, ECMWF
-
-    http://www.math.colostate.edu/~gerhard/MATH540/FILES/
-    Karimi2010_ChaosInLorenz96.pdf
     '''
     
     args = ParseCmdLine()
     
-    Lorenz96( args )
+    Rossler( args )
     
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
-def Lorenz96( args ):
+def Rossler( args ):
     '''
     Use scipy.integrate.odeint() to integrate the differential equation.
 
@@ -53,27 +32,24 @@ def Lorenz96( args ):
     and even with that: I could not get it to work... Nice. 
     '''
     
-    # initial state (equilibrium)
-    v0 = args.forceConstant * np.ones( args.nVariables )
-
-    v0[ args.i_perturb ] += args.perturb # add small perturbation
+    # initial state
+    v0 = np.array( args.initial )
 
     t = np.arange( 0.0, args.T, args.dT )
 
     # odeint requires "extra" variables to be in a tuple with name matching
     # inside the derivative function, so make N, F explicit
-    N = args.nVariables
-    F = args.forceConstant
-    V = odeint( dLorenz96, v0, t, args = (N, F) )
+    a, b, c = args.constants
+    V = odeint( dRossler, v0, t, args = (a,b,c) )
 
     # Compute and store the derivatives
     dVdt      = np.zeros( V.shape )
     dVdt_cols = range( dVdt.shape[1] )
 
     for row in range( 0, dVdt.shape[0] ):
-        # State variable derivatives dV/dt directly from Lorenz96()
+        # State variable derivatives dV/dt directly from dRossler()
         # using the integrated value at each timestep as the v0
-        dVdt[ row, dVdt_cols ] = dLorenz96( V[ row, dVdt_cols ], 0, N, F )
+        dVdt[ row, dVdt_cols ] = dRossler( V[ row, dVdt_cols ], 0, a,b,c )
 
     if len( args.jacobians ) :
         # Note that args.jacobians is a list of pairs (tuples)
@@ -85,19 +61,15 @@ def Lorenz96( args ):
             jacobians[ :, col ] = d1 / d2
 
     # Create output matrix
-    # get index of starting point to exclude transient start
-    exclude = int( np.where( t == args.exclude )[0] )
-    
     # t is an array, first cast as matrix and transpose to merge with x & dVdt
     output = np.concatenate( (np.asmatrix( t ).T, V, dVdt), 1 )
     if len( args.jacobians ) :
         output = np.concatenate( (output, jacobians), 1 )
-    output = output[ exclude::, ]
 
     if args.outputFile:
         # Create the header
         header = 'Time'
-        for dim in range( args.nVariables ) :
+        for dim in range( len( v0 ) ) :
             header = header + ',V' + str( dim + 1 )
         for dim in dVdt_cols :
             header = header + ',dV' + str( dim + 1 ) + '/dt'
@@ -116,9 +88,9 @@ def Lorenz96( args ):
       fig3D = plt.figure()
       ax3D  = fig3D.gca(projection='3d')
       
-      ax3D.plot( V[ exclude::, args.dimensions[0] ],
-                 V[ exclude::, args.dimensions[1] ],
-                 V[ exclude::, args.dimensions[2] ] )
+      ax3D.plot( V[ :, args.dimensions[0] ],
+                 V[ :, args.dimensions[1] ],
+                 V[ :, args.dimensions[2] ] )
       
       ax3D.set_xlabel( '$x_{0:d}$'.format( args.dimensions[0] ) )
       ax3D.set_ylabel( '$x_{0:d}$'.format( args.dimensions[1] ) )
@@ -139,80 +111,58 @@ def Lorenz96( args ):
 
       ax.set( xlabel = 'index ()',
               ylabel = 'amplitude ()',
-              title  = 'Lorenz 96' )
+              title  = 'Rossler' )
       plt.show()
 
 #----------------------------------------------------------------------------
 #  State derivatives
-#  Relies on Python array wrapping for negative/overflow indicies
 #----------------------------------------------------------------------------
-def dLorenz96( v, t, N, F ):
+def dRossler( v, t, a, b, c ):
     '''
-    N variables k=1, ... N, governed by N equations:
-           d X_k / dt = (X_k+1 - X_k-2) * X_k-1 - X_k + F
-    Assume N > 3
+    dx/dt = -y - z
+    dy/dt =  x + ay
+    dz/dt =  b + z(x-c)
     '''
-    dv = np.zeros(N)
-
-    # first the 3 edge cases: i=1,2,N
-    dv[0]   = (v[1] - v[N-2]) * v[N-1] - v[0]
-    dv[1]   = (v[2] - v[N-1]) * v[0]   - v[1]
-    dv[N-1] = (v[0] - v[N-3]) * v[N-2] - v[N-1]
+    dv = np.zeros( len( v ) )
+    x,y,z = v
     
-    # then the general case
-    for i in range(2, N-1):
-        dv[i] = (v[i+1] - v[i-2]) * v[i-1] - v[i]
-
-    # add the forcing term
-    dv = dv + F
-
+    # 
+    dv[0] = -y - z
+    dv[1] =  x + a*y  
+    dv[2] =  b + z*(x-c)
+    
     return dv
 
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 def ParseCmdLine():
     
-    parser = ArgumentParser( description = 'Lorenz 96' )
+    parser = ArgumentParser( description = 'Rossler' )
     
-    parser.add_argument('-D', '--nVariables',
-                        dest   = 'nVariables', type = int, 
-                        action = 'store',     default = 5,
-                        help = 'Number of variables.')
-    
+    parser.add_argument('-i', '--initial', nargs = '+',
+                        dest   = 'initial', type = float,
+                        action = 'store', default = [1, 0, 1],
+                        help = 'Initial state values.')
+
+    parser.add_argument('-c', '--constants', nargs = '+',
+                        dest   = 'constants', type = float,
+                        action = 'store', default = [0.2, 0.2, 5.7],
+                        help = 'Constants a, b, c.')
+
     parser.add_argument('-j', '--jacobians', nargs = '+',
                         dest   = 'jacobians',
                         action = 'store', default = [],
                         help = 'Variable Jacobian columns, list of pairs.')
 
-    parser.add_argument('-f', '--forceConstant',
-                        dest   = 'forceConstant', type = int, 
-                        action = 'store',      default = 8,
-                        help = 'Forcing constant.')
-
-    parser.add_argument('-p', '--perturb',
-                        dest   = 'perturb', type = float, 
-                        action = 'store',      default = 0.01,
-                        help = 'Pertubation value.')
-
-    parser.add_argument('-i', '--iPerturb',
-                        dest   = 'i_perturb', type = int, 
-                        action = 'store',      default = 3,
-                        help = 'Pertubation index.')
-
     parser.add_argument('-T', '--T',
                         dest   = 'T', type = float, 
-                        action = 'store',      default = 60.,
+                        action = 'store',      default = 100.,
                         help = 'Max time.')
 
     parser.add_argument('-t', '--dT',
                         dest   = 'dT', type = float, 
-                        action = 'store',      default = 0.05,
+                        action = 'store',      default = 0.025,
                         help = 'Time increment.')
-
-    parser.add_argument('-x', '--exclude',
-                        dest   = 'exclude', type = float, 
-                        action = 'store',      default = 10.,
-                        help = 'Initial transient time to exclude.')
 
     parser.add_argument('-o', '--outputFile',
                         dest   = 'outputFile', type = str, 
@@ -233,10 +183,6 @@ def ParseCmdLine():
 
     # Zero offset dimensions
     args.dimensions = [ d-1 for d in args.dimensions ]
-
-    if args.i_perturb >= args.nVariables :
-        print( "i_perturb > D, setting to 1" )
-        args.i_perturb = 1
 
     if len( args.jacobians ) :
         # Must be pairs of coefficient columns